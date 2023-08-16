af40ce3ac0f392baf8792285a354cae9283b565e
ASUSVIVOBOOKSM\samir <samir.mamedzade211@gmail.com>
2023-08-17 00:43:48 +0400

    responsive slider added

--- app.js
+++ app.js
@@ -11,6 +11,43 @@ let acordionShop = false;
 let currentElement = document.createElement('div');
 let currentElementShop = document.createElement('div');
 
+
+// slider
+let nextBtn = document.getElementById("right");
+let prevBtn = document.getElementById("left");
+let sliderItem = document.querySelectorAll(".jsc-slider-item");
+let counter = 0;
+
+
+nextBtn.addEventListener('click', nextSlide);
+
+function nextSlide() {
+    sliderItem[counter].style.animation = "next1 1s ease-in forwards";
+    if(counter >=  sliderItem.length-1){
+        counter=0; 
+    }
+    else{
+        counter++; 
+    }
+    sliderItem[counter].style.animation = "next2 1s ease-in forwards";
+
+ 
+}
+
+// nextSlide = () =>{
+//     this.move += 640;
+//        if(this.move >= 640 * Array.from(this.images).length){
+//         this.move=0;
+//        }
+//         Array.from(this.images).forEach(x => {
+//             x.style.left = -this.move + "px"
+//         })
+//         console.log( this.move);
+//    }
+
+
+
+
 //nav bar fixed listener
 window.onscroll = function () { navbarSticky() };
 //mobil bar open listener
