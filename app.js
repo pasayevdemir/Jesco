a74e035ac9c3220102e42a9c9d15ef77dc299fe0
DamirPashayev <83781955+pasayevdemir@users.noreply.github.com>
2023-08-19 13:25:34 +0400

    product and watch

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 var navbar = document.getElementById("stickyBar");
 var sticky = navbar.offsetTop;
 var iconMenu = document.querySelector(".jsc-icon-menu");
+var ratingNum = document.querySelector(".rating-num").textContent.replace(" Review )", ' ').replace('(', ' ');
+var starColor = document.querySelector(".star-color");
 let mobilList = document.querySelectorAll(".plus-icon");
 var mobilBtnClose = document.getElementById("mobilBtnClose");
 const mobilBar = document.getElementById("mobilBar");
@@ -11,7 +13,13 @@ let acordionShop = false;
 let currentElement = mobilList[0].children[1];
 let currentElementShop = mobilList[0].children[1];
 
+//clock
+let timeEnd = new Date("Dec 31 2023 00:00:00");
+clock();
+setInterval(() => { clock() }, 1000);
 
+//star rating
+starColor.style.width = ratingNum * 19.5 + '%';
 
 
 // slider
@@ -20,34 +28,9 @@ let prevBtn = document.getElementById("left");
 let sliderItem = document.querySelectorAll(".jsc-slider-item");
 let counter = 0;
 
-
+//Slider listener
 nextBtn.addEventListener('click', nextSlide);
 prevBtn.addEventListener('click', prevSlide);
-
-function nextSlide() {
-    sliderItem[counter].style.animation = "next1 1s ease-in forwards";
-    if(counter >=  sliderItem.length-1){
-        counter=0; 
-    }
-    else{
-        counter++; 
-    }
-    sliderItem[counter].style.animation = "next2 1s ease-in forwards";
-}
-function prevSlide() {
-    sliderItem[counter].style.animation = "next2 1s ease-in forwards";
-    if(counter == 0){
-        counter= sliderItem.length-1; 
-    }
-    else{
-        counter--; 
-    }
-    sliderItem[counter].style.animation = "next1 1s ease-in forwards";
-}
-
-
-
-
 //nav bar fixed listener
 window.onscroll = function () { navbarSticky() };
 //mobil bar open listener
@@ -92,6 +75,40 @@ mobilList.forEach((x) => {
 
 
 //Function
+function clock() {
+    time = (timeEnd - new Date()) / 1000;
+    day = Math.floor(time / 3600 / 24);
+    hour = Math.floor(time / 3600) % 24;
+    min = Math.floor(time / 60) % 60
+    sec = Math.floor(time) % 60;
+    document.getElementById("dayId").textContent = day < 10 ? "0" + day : day;
+    document.getElementById("hourId").textContent = hour < 10 ? "0" + hour : hour;
+    document.getElementById("minId").textContent = min < 10 ? "0" + min : min;
+    document.getElementById("secId").textContent = sec < 10 ? "0" + sec : sec;
+}
+
+function nextSlide() {
+    sliderItem[counter].style.animation = "next1 1s ease-in forwards";
+    if (counter >= sliderItem.length - 1) {
+        counter = 0;
+    }
+    else {
+        counter++;
+    }
+    sliderItem[counter].style.animation = "next2 1s ease-in forwards";
+}
+function prevSlide() {
+    sliderItem[counter].style.animation = "next2 1s ease-in forwards";
+    if (counter == 0) {
+        counter = sliderItem.length - 1;
+    }
+    else {
+        counter--;
+    }
+    sliderItem[counter].style.animation = "next1 1s ease-in forwards";
+}
+
+
 function iconPlus(elemPlus) {
     elemPlus.parentElement.children[0].classList += " active-minus";
 
@@ -126,13 +143,13 @@ function mobilBarOpen() {
     mobilBar.style.left = "0px";
     document.querySelector("body").style.overflow = "hidden";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-    document.querySelector(".body-blocked").style.zIndex= "6";
+    document.querySelector(".body-blocked").style.zIndex = "6";
 }
 function mobilBarClose() {
     mobilBar.style.left = "-400px";
     document.querySelector("body").style.overflow = "auto";
     document.body.style.backgroundColor = "white";
-    document.querySelector(".body-blocked").style.zIndex= "-777";
+    document.querySelector(".body-blocked").style.zIndex = "-777";
 }
 
 
